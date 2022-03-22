24975624aff44ab8187c39f761dc634d3b6e32af
LinKa <maltseva-as@mail.ru>
2022-03-22 10:55:30 +0300

    models added. model meeting has questions...

--- proj/models.py
+++ proj/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    login = models.CharField(max_length=50)
+    password = models.CharField()
+
+
+class Meeting(models.Model):
+    location = models.CharField()
+    start_time = models.DateTimeField()
+    interval = models.DateTimeField()
+
+    
+class Day(models.Model):
+    DAY_OF_WEEK = [
+        ('Mon', 'Monday'),
+        ('Tue', 'Tuesday'),
+        ('Wed', 'Wednesday'),
+        ('Th', 'Thursday'),
+        ('Fr', 'Friday'),
+        ('Sat', 'Saturday'),
+        ('Sun', 'Sunday'),
+    ]
+    user_id = models.BigIntegerField()
+    meetings = ArrayField(
+        ArrayField(Meeting)
+    )
